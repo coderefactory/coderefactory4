ec421e0ccbc8f62ba67f4d6114afeb004aaadd91
Andrew dePasquale <andy@webakimbo.com>
2020-12-01 11:18:11 -0800

    first pass completed animation POCs.

--- src/assets/scripts/modules/animations.js
+++ src/assets/scripts/modules/animations.js
@@ -37,21 +37,6 @@ const initJigsawAnimation = () => {
   const jigsawBox = document.getElementById('jigsaw-box');
   const pieces = jigsawBox.getElementsByClassName('icon-chat-bubble');
 
-  // define variables
-  const spinAnimationOpts = {
-    duration: 0.75,
-    ease: CustomEase.create('custom', 'M0,0 C0.005,0.018 0.02,-0.088 0.18,-0.088 0.201,-0.088 0.289,-0.09 0.304,-0.09 0.543,-0.09 0.347,0.867 0.736,0.976 0.822,1 0.818,1.001 1,1'),
-    transformOrigin: 'center'
-  };
-
-  const morphAnimationOpts = {
-    duration: 0.25,
-    delay: 0.4,
-    ease: 'elastic.out(1, 0.6)',
-    transformOrigin: 'center',
-    paused: true
-  };
-
   // define helpers
   const partitionPaths = paths => {
     return Array.from(paths).reduce(
@@ -85,31 +70,83 @@ const initJigsawAnimation = () => {
     let tween = gsap.to(fromSvg, {
       morphSVG: toSvg,
       fill: getFillColor(toSvg),
+      stroke: getFillColor(toSvg),
+      strokeWidth: 3,
       rotation: i * 90,
-      ...getTranslation(i),
-      ...morphAnimationOpts
+      duration: 0.25,
+      ease: 'power4.out',
+      transformOrigin: 'center',
+      paused: true,
+      ...getTranslation(i)
     });
 
     piece.addEventListener('tween.start', () => tween.play());
+
+    gsap.set(piece, { scale: 0 });
   }
 
   // choreograph full animation
-  const spin = gsap.to(jigsawBox, {
-    rotation: 360,
-    ...spinAnimationOpts
-  });
-  Array.from(pieces).forEach((piece, i) => {
-    piece.dispatchEvent(new CustomEvent('tween.start'));
-  });
+  const bubbles = () => {
+    const tl = gsap.timeline();
+    tl.to(Array.from(pieces), {
+      transformOrigin: 'center',
+      scale: 1,
+      ease: 'elastic.out(1, 0.5)',
+      stagger: {
+        each: 0.2
+      }
+    });
+    return tl;
+  };
+  const spin = () => {
+    const tl = gsap.timeline();
+    tl.to(jigsawBox, {
+      rotation: 360,
+      duration: 0.75,
+      ease: CustomEase.create('custom', 'M0,0 C0.005,0.018 0.02,-0.088 0.18,-0.088 0.201,-0.088 0.289,-0.09 0.304,-0.09 0.543,-0.09 0.347,0.867 0.736,0.976 0.822,1 0.818,1.001 1,1'),
+      transformOrigin: 'center'
+    });
+    return tl;
+  };
+  const morph = () => {
+    const tl = gsap.timeline();
+    Array.from(pieces).forEach((piece, i) => {
+      piece.dispatchEvent(new CustomEvent('tween.start'));
+    });
+    return tl;
+  };
+  const masterTimeline = gsap.timeline();
+  masterTimeline.add(bubbles)
+                .add(spin, "+=1.5")
+                .add(morph, "+=0.3");
 };
 
 // monitor
 const initMonitorAnimation = () => {
+  // define elements
+  const monitorBox = document.getElementById('monitor-box');
+  const gem = document.querySelector('.gem');
+
+  // build animation
+  gsap.to(monitorBox, {
+    duration: 0.4,
+    ease: 'power4.inOut',
+    rotationX: 14,
+    rotationY: -34,
+    rotationZ: -8,
+    transformOrigin: 'center'
+  });
+
+  const gemTimeline = gsap.timeline({ delay: 0.35 });
+  gemTimeline.to(gem, { display: 'block' }, 'reveal');
+  gemTimeline.to(gem, { y: -40, duration: 0.25, ease: 'power4.out' }, 'reveal');
+  gemTimeline.to(gem, { y: 0, duration: 0.35, ease: 'bounce.out' }, 'reveal+=0.25');
+  gemTimeline.to(gem, { rotationY: -360, duration: 1 }, 'reveal');
 };
 
 // init onload
 document.addEventListener('DOMContentLoaded', () => {
-  initGearsAnimation();
-  initJigsawAnimation();
-  // initMonitorAnimation();
+  setTimeout(initGearsAnimation, 100);
+  setTimeout(initJigsawAnimation, 4000);
+  setTimeout(initMonitorAnimation, 7000);
 });
