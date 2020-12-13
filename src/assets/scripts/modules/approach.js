8faf30b7d8d17b63912777518e056ec4b006c41a
Andrew dePasquale <andy@webakimbo.com>
2020-12-13 15:54:47 -0800

    Implemented poc for Approach page - no animations.

---
+++ src/assets/scripts/modules/approach.js
@@ -0,0 +1,22 @@
+import 'waypoints/lib/noframework.waypoints';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // get elements
+  const main = document.getElementsByTagName('main')[0];
+  const section = document.getElementById('approach');
+  if (!section) {
+    return; 
+  }
+
+  // set up for animation
+  section.classList.remove('animated');
+  const wpApproach = new Waypoint({
+    context: main,
+    element: section,
+    handler: direction => {
+      section.classList.add('animated');
+      wpApproach.destroy();
+    },
+    offset: '10vh'
+  });
+});
